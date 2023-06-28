3e57decf6459fc42f7ade0567b2f350c6a02aa47
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-06-28 17:46:47 -0300

    feat: carrossel no banner

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+      delay: 2500, 
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
