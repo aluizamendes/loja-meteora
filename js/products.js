d611bd4529ef38028c3d76b5249729b19c0e637c
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-06-29 18:40:44 -0300

    refactor: add cores e tamanho dos produtos dinamicamente e ajustando na exibicao do modal

--- js/products.js
+++ js/products.js
@@ -4,6 +4,15 @@ const productList = document.querySelector("[data-lista-produtos]")
 const products =  [
     {
         id: "1",
+        // colors: ["Azul claro", "Offwhite", "Preto", "Mescla"],
+        colors: {
+            "Mostarda": "#e69d1e",
+            "Azul claro": "#6eaaff",
+            "OffWhite": "#f0f2f5",
+            "Preto": "#000",
+            "Mescla": "#999999"
+        },
+        size: ["P", "PP", "M", "G", "GG"],
         imageURL: "assets/Desktop/Imagens Cards/Camiseta.png",
         title: "Camiseta Conforto",
         description: "Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.",
@@ -11,6 +20,13 @@ const products =  [
     },
     {
         id: "2",
+        colors: {
+            "Caqui": "#fce7c2",
+            "Azul escuro": "#1f0561",
+            "OffWhite": "#f0f2f5",
+            "Preto": "#000"
+        },
+        size: ["P", "PP", "M", "G", "GG"],
         imageURL: "assets/Desktop/Imagens Cards/Calca.png",
         title: "Calça Alfaiataria",
         description: "Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!",
@@ -18,6 +34,12 @@ const products =  [
     },
     {
         id: "3",
+        colors: {
+            "Branco": "#FFF",
+            "OffWhite": "#f0f2f5",
+            "Preto": "#000"
+        },
+        size: ["34", "35", "36", "37", "38", "39", "40"],
         imageURL: "assets/Desktop/Imagens Cards/Tenis.png",
         title: "Tênis Chunky",
         description: "Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.",
@@ -25,6 +47,13 @@ const products =  [
     },
     {
         id: "4",
+        colors: {
+            "Azul escuro": "#1f0561",
+            "Azul claro": "#6eaaff",
+            "Rosa": "#f587b6",
+            "Preto": "#000"
+        },
+        size: ["P", "PP", "M", "G", "GG"],
         imageURL: "assets/Desktop/Imagens Cards/Jaqueta.png",
         title: "Jaqueta Jeans",
         description: "Modelo unissex oversized com gola de camurça. Atemporal e autêntica!",
@@ -32,6 +61,13 @@ const products =  [
     },
     {
         id: "5",
+        colors: {
+            "Dourado": "#c4ae08",
+            "Marrom": "#78513e",
+            "Prata": "#999999",
+            "Preto": "#000"
+        },
+        size: ["Único"],
         imageURL: "assets/Desktop/Imagens Cards/oculos.png",
         title: "Óculos Redondo",
         description: "Armação metálica em grafite com lentes arredondadas. Sem erro!",
@@ -39,6 +75,14 @@ const products =  [
     },
     {
         id: "6",
+        colors: ["Bege", "Rosa antigo", "Branco", "Preto"],
+        colors: {
+            "Bege": "#f7ab8b",
+            "Rosa antigo": "#b3568a",
+            "Branco": "#FFF",
+            "Preto": "#000"
+        },
+        size: ["Único"],
         imageURL: "assets/Desktop/Imagens Cards/Bolsa.png",
         title: "Bolsa coringa",
         description: "Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!",
@@ -69,11 +113,25 @@ function renderCard(product) {
 }
 
 function renderProductModal(product) {
+    const colors = product.colors
+    const sizes = product.size
     const image = product.imageURL
     const title = product.title
     const description = product.description
     const price = Number(product.price).toFixed(2).replace(".", ",")
 
+    // salvar nome das cores em um array
+    const colorNames = Object.keys(colors)
+
+    // iterar o colorNames para retornar um array de objetos com o nome da cor e o valor hex dela
+    const colorMap = colorNames.map((colorName) => {
+        return {
+            name: colorName,
+            colorValue: colors[colorName]
+        }
+    })
+
+    // div pra colcar o dialog dentro
     const div = document.querySelector(".modal-produto")
 
     div.innerHTML = `
@@ -99,18 +157,18 @@ function renderProductModal(product) {
                         <div class=product__content__middle>
                             <span class=product__content__middle-title>Cores:</span>
                             <div>
-                                <div class="radio-field">
-                                    <input type="radio" name="cor" id="azulClaro">
-                                    <label for="azulClaro">Azul claro</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="cor" id="offWhite">
-                                    <label for="offWhite">Off-white</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="cor" id="preto">
-                                    <label for="preto">Preto</label>
-                                </div>
+                                ${
+                                    colorMap.map((color) => {
+                                        return `
+                                        <div class="radio-field">
+                                            <input type="radio" name="cor" id="${ color.name.split(" ").join("") }" style="background-color: ${ color.colorValue };">
+                                            <label for="${ color.name.split(" ").join("") }">${ color.name }</label>
+                                        </div>
+                                        `
+                                    }).join("")
+                                }
+
+                                
                             </div>
 
                             
@@ -119,26 +177,16 @@ function renderProductModal(product) {
                         <div class=product__content__bottom>
                             <span class=product__content__bottom-title>Tamanho:</span>
                             <div>
-                                <div class="radio-field">
-                                    <input type="radio" name="tamanho" id="p">
-                                    <label for="p">P</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="tamanho" id="pp">
-                                    <label for="pp">PP</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="tamanho" id="m">
-                                    <label for="m">M</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="tamanho" id="g">
-                                    <label for="g">G</label>
-                                </div>
-                                <div class="radio-field">
-                                    <input type="radio" name="tamanho" id="gg">
-                                    <label for="gg">GG</label>
-                                </div>
+                                ${
+                                    sizes.map((size) => {
+                                        return`
+                                        <div class="radio-field">
+                                            <input type="radio" name="tamanho" id="${size}">
+                                            <label for="${size}">${size}</label>
+                                        </div>
+                                        `
+                                    }).join("")
+                                }
                             </div>                                
                         </div>
                         <button>Adicionar à sacola</button>
